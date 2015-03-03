98e02b696af71a7f5f1351c85d1cc0a445457d10
Tysonzero <danielspencersmith@gmail.com>
2015-03-03 13:00:57 -0700

    Create systems.move

---
+++ scripts/systems/move.ts
@@ -0,0 +1,13 @@
+/// <reference path='../entities.ts'/>
+
+module systems {
+    export function move() {
+        var i;
+        for (i = entities.length - 1; i >= 0; i--) {
+            if (entities[i].position && entities[i].velocity) {
+                entities[i].position.x += entities[i].velocity.x;
+                entities[i].position.y += entities[i].velocity.y;
+            }
+        }
+    };
+}
