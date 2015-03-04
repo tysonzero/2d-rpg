3941f90fd735facc639c0c3b5e143e41329218ae
Tysonzero <danielspencersmith@gmail.com>
2015-03-03 22:50:45 -0700

    Add constructor to Entity

--- scripts/entities.ts
+++ scripts/entities.ts
@@ -4,6 +4,11 @@
 class Entity {
     position: components.Position;
     velocity: components.Velocity;
+    constructor(options?: {position?: components.Position; velocity?: components.Velocity}) {
+        options = options || {};
+        this.position = options.position;
+        this.velocity = options.velocity;
+    }
 }
 
 var entities: Entity[] = [];
