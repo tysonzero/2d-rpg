73ce7958e63eabc02723363782b37feb8b19e6ad
Tysonzero <danielspencersmith@gmail.com>
2015-03-05 23:50:48 -0700

    Create render system

---
+++ scripts/systems/render.ts
@@ -0,0 +1,19 @@
+/// <reference path='../entities.ts'/>
+/// <reference path='../context.ts'/>
+
+module systems {
+    export function render() {
+        var i;
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        for (i = entities.length - 1; i >= 0; i--) {
+            if (entities[i].position && entities[i].sprite) {
+                context.fillRect(
+                    entities[i].position.x - entities[i].sprite.width / 2,
+                    entities[i].position.y - entities[i].sprite.height / 2,
+                    entities[i].sprite.width,
+                    entities[i].sprite.height
+                );
+            }
+        }
+    }
+}
