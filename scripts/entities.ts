3d50be93cc671961e1309204ce0e8fba07d08eda
Tysonzero <danielspencersmith@gmail.com>
2015-03-05 23:47:43 -0700

    Add sprite component to Entity

--- scripts/entities.ts
+++ scripts/entities.ts
@@ -1,5 +1,6 @@
 /// <reference path='components/position.ts'/>
 /// <reference path='components/velocity.ts'/>
+/// <reference path='components/sprite.ts'/>
 
 class Entity {
     position: components.Position;
@@ -9,6 +10,7 @@ class Entity {
         this.position = options.position;
         this.velocity = options.velocity;
     }
+    sprite?: components.Sprite;
 }
 
 var entities: Entity[] = [];
