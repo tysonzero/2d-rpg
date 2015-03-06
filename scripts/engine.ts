c87712f9f96a71b1401eb6ed79fdec604c5feb1b
Tysonzero <danielspencersmith@gmail.com>
2015-03-05 23:51:20 -0700

    Call systems.render() in engine.ts

--- scripts/engine.ts
+++ scripts/engine.ts
@@ -1,7 +1,9 @@
 /// <reference path='systems/move.ts'/>
+/// <reference path='systems/render.ts'/>
 
 function tick() {
     systems.move();
+    systems.render();
     window.requestAnimationFrame(tick);
 }
 window.requestAnimationFrame(tick);
