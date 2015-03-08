fe28b9cac6bd03aa1a6c3b13721f2dc3a187e8b6
Tysonzero <danielspencersmith@gmail.com>
2015-03-07 23:54:07 -0700

    Set fillStyle to sprite.color

--- scripts/systems/render.ts
+++ scripts/systems/render.ts
@@ -9,7 +9,7 @@ module systems {
         context.fillRect(0, 0, context.canvas.width, context.canvas.height);
         for (i = entities.length - 1; i >= 0; i--) {
             if (entities[i] && entities[i].position && entities[i].sprite) {
-                context.fillStyle = '#000000'
+                context.fillStyle = entities[i].sprite.color;
                 context.fillRect(
                     entities[i].position.x - entities[i].sprite.width / 2,
                     entities[i].position.y - entities[i].sprite.height / 2,
