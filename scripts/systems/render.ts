623d1439fb7a8179155d53692748a6882971dc05
Tysonzero <danielspencersmith@gmail.com>
2015-03-07 21:50:28 -0700

    Set canvas's background color to white

--- scripts/systems/render.ts
+++ scripts/systems/render.ts
@@ -5,8 +5,11 @@ module systems {
     export function render() {
         var i;
         context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        context.fillStyle = '#FFFFFF'
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
         for (i = entities.length - 1; i >= 0; i--) {
             if (entities[i].position && entities[i].sprite) {
+                context.fillStyle = '#000000'
                 context.fillRect(
                     entities[i].position.x - entities[i].sprite.width / 2,
                     entities[i].position.y - entities[i].sprite.height / 2,
