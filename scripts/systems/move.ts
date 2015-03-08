a7c390a99ff91b12369cb7547f8b9598be159fc2
Tysonzero <danielspencersmith@gmail.com>
2015-03-07 23:05:21 -0700

    Check if entity exists before component checks

--- scripts/systems/move.ts
+++ scripts/systems/move.ts
@@ -4,7 +4,7 @@ module systems {
     export function move(delta: number) {
         var i;
         for (i = entities.length - 1; i >= 0; i--) {
-            if (entities[i].position && entities[i].velocity) {
+            if (entities[i] && entities[i].position && entities[i].velocity) {
                 entities[i].position.x += delta * entities[i].velocity.x;
                 entities[i].position.y += delta * entities[i].velocity.y;
             }
--- scripts/systems/render.ts
+++ scripts/systems/render.ts
@@ -8,7 +8,7 @@ module systems {
         context.fillStyle = '#FFFFFF'
         context.fillRect(0, 0, context.canvas.width, context.canvas.height);
         for (i = entities.length - 1; i >= 0; i--) {
-            if (entities[i].position && entities[i].sprite) {
+            if (entities[i] && entities[i].position && entities[i].sprite) {
                 context.fillStyle = '#000000'
                 context.fillRect(
                     entities[i].position.x - entities[i].sprite.width / 2,
