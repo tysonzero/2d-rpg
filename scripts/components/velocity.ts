5cc3eb3aaa33692f5a90e48826087513fa546a3c
Tysonzero <danielspencersmith@gmail.com>
2015-03-08 23:18:14 -0600

    Finish replacing classes with interfaces

--- scripts/components/velocity.ts
+++ scripts/components/velocity.ts
@@ -1,11 +1,6 @@
 module components {
-    export class Velocity {
+    export interface Velocity {
         x: number;
         y: number;
-        constructor(options?: {x?: number; y?: number}) {
-            options = options || {};
-            this.x = options.x || 0;
-            this.y = options.y || 0;
-        }
     }
 }
