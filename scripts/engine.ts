742f2375761d3a409d60ca9faf52a23ccacb4641
Tysonzero <danielspencersmith@gmail.com>
2015-03-09 23:53:11 -0600

    Call systems.input

--- scripts/engine.ts
+++ scripts/engine.ts
@@ -1,11 +1,13 @@
 /// <reference path='systems/move.ts'/>
 /// <reference path='systems/render.ts'/>
+/// <reference path='systems/input.ts'/>
 
 var engine = {
     time: performance.now(),
     tick: () => {
         var delta = Math.min((performance.now() - engine.time) / 1000, 0.05);
         engine.time = performance.now();
+        systems.input(delta);
         systems.move(delta);
         systems.render();
         window.requestAnimationFrame(engine.tick);
