1dfc0798482577d6e47d429b0045f41f14aef073
Tysonzero <danielspencersmith@gmail.com>
2015-03-09 23:43:59 -0600

    Create Input component

---
+++ scripts/components/input.ts
@@ -0,0 +1,9 @@
+module components {
+    export interface Input {
+        acceleration: number;
+        upKey: number;
+        downKey: number;
+        leftKey: number;
+        rightKey: number;
+    }
+}
