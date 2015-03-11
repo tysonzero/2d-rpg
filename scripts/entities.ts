db67442536f74cff49f8785571fddafe7caaab0f
Tysonzero <danielspencersmith@gmail.com>
2015-03-10 23:56:28 -0600

    Add second player with different controls
    Might end up with only one player, but it will be fun to experiment
    with two for the time being.

--- scripts/entities.ts
+++ scripts/entities.ts
@@ -14,5 +14,11 @@ var entities: {
         velocity: {x: 50, y: 50},
         sprite: {width: 50, height: 50, color: '#FF0000', shape: 'ellipse'},
         input: {acceleration: 200, upKey: 87, downKey: 83, leftKey: 65, rightKey: 68}
+    },
+    {
+        position: {x: 400, y: 300},
+        velocity: {x: 50, y: 50},
+        sprite: {width: 50, height: 50, color: '#0000FF', shape: 'ellipse'},
+        input: {acceleration: 200, upKey: 73, downKey: 75, leftKey: 74, rightKey: 76}
     }
 ];
