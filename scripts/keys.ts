9cc7c08528bf108a05c507efa26fae12fc939e15
Tysonzero <danielspencersmith@gmail.com>
2015-03-10 20:09:47 -0600

    Move event listeners from input.ts to keys.ts

--- scripts/keys.ts
+++ scripts/keys.ts
@@ -3,3 +3,29 @@ var keys: {
     clicked: number;
     released: number;
 }[] = [];
+
+window.addEventListener('keydown', function (event) {
+    if (!keys[event.keyCode]) {
+        keys[event.keyCode] = {
+            down: true,
+            clicked: 1,
+            released: 0
+        }
+    }
+    keys[event.keyCode].clicked++;
+    keys[event.keyCode].down = true;
+    event.preventDefault();
+    return false;
+});
+
+window.addEventListener('keyup', function (event) {
+    if (!keys[event.keyCode]) {
+        keys[event.keyCode] = {
+            down: false,
+            clicked: 0,
+            released: 1
+        }
+    }
+    keys[event.keyCode].released++;
+    keys[event.keyCode].down = false;
+});
--- scripts/systems/input.ts
+++ scripts/systems/input.ts
@@ -1,30 +1,6 @@
 /// <reference path='../entities.ts'/>
 /// <reference path='../keys.ts'/>
 
-window.addEventListener('keydown', function (event) {
-    if (!keys[event.keyCode]) {
-        keys[event.keyCode] = {
-            down: true,
-            clicked: 1,
-            released: 0
-        }
-    }
-    keys[event.keyCode].clicked++;
-    keys[event.keyCode].down = true;
-});
-
-window.addEventListener('keyup', function (event) {
-    if (!keys[event.keyCode]) {
-        keys[event.keyCode] = {
-            down: false,
-            clicked: 0,
-            released: 1
-        }
-    }
-    keys[event.keyCode].released++;
-    keys[event.keyCode].down = false;
-});
-
 module systems {
     export function input(delta: number) {
         var i: number;
