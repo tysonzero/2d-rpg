a0477c918de411479f84abe62894d43c6687fbec
Tysonzero <danielspencersmith@gmail.com>
2015-03-12 13:01:29 -0600

    Call systems.resetContext

--- scripts/engine.ts
+++ scripts/engine.ts
@@ -1,6 +1,7 @@
 /// <reference path='systems/input.ts'/>
 /// <reference path='systems/move.ts'/>
 /// <reference path='systems/render.ts'/>
+/// <reference path='systems/reset-context.ts'/>
 /// <reference path='systems/update-time.ts'/>
 
 var engine = {
@@ -8,6 +9,7 @@ var engine = {
         systems.updateTime();
         systems.input();
         systems.move();
+        systems.resetContext();
         systems.render();
         window.requestAnimationFrame(engine.tick);
     }
