23014364ad5b98ee8d4eacb1203ea519b073086a
Tysonzero <danielspencersmith@gmail.com>
2015-03-14 23:19:10 -0600

    Utilize walk.speed in walk system

--- scripts/systems/walk.ts
+++ scripts/systems/walk.ts
@@ -5,20 +5,36 @@
 module systems {
     export function walk() {
         var i: number;
+        var up: number;
+        var down: number;
+        var left: number;
+        var right: number;
+        var desired: {x: number; y: number};
+        var change: {x: number; y: number};
         for (i = entities.length - 1; i >= 0; i--) {
             if (entities[i] && entities[i].walk) {
-                if (keys.down[entities[i].walk.keys.up]) {
-                    entities[i].velocity.y -= time.delta * entities[i].walk.acceleration;
+                up = keys.down[entities[i].walk.keys.up] ? 1 : 0;
+                down = keys.down[entities[i].walk.keys.down] ? 1 : 0;
+                left = keys.down[entities[i].walk.keys.left] ? 1 : 0;
+                right = keys.down[entities[i].walk.keys.right] ? 1 : 0;
+                desired = {
+                    x: (right - left) * ((down - up) ? Math.sqrt(2) / 2 : 1),
+                    y: (down - up) * ((right - left) ? Math.sqrt(2) / 2 : 1)
+                };
+                desired.x *= entities[i].walk.speed;
+                desired.y *= entities[i].walk.speed;
+                change = {
+                    x: desired.x - entities[i].velocity.x,
+                    y: desired.y - entities[i].velocity.y
+                };
+                if (Math.sqrt(Math.pow(change.x, 2) + Math.pow(change.y, 2)) > time.delta * entities[i].walk.acceleration) {
+                    change.x /= Math.sqrt(Math.pow(change.x, 2) + Math.pow(change.y, 2));
+                    change.y /= Math.sqrt(Math.pow(change.x, 2) + Math.pow(change.y, 2));
+                    change.x *= time.delta * entities[i].walk.acceleration;
+                    change.y *= time.delta * entities[i].walk.acceleration;
                 }
-                if (keys.down[entities[i].walk.keys.down]) {
-                    entities[i].velocity.y += time.delta * entities[i].walk.acceleration;
-                }
-                if (keys.down[entities[i].walk.keys.left]) {
-                    entities[i].velocity.x -= time.delta * entities[i].walk.acceleration;
-                }
-                if (keys.down[entities[i].walk.keys.right]) {
-                    entities[i].velocity.x += time.delta * entities[i].walk.acceleration;
-                }
+                entities[i].velocity.x += change.x;
+                entities[i].velocity.y += change.y;
             }
         }
     }
