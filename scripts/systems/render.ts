4cfae39185acc88bb9af097f7cce3d3c5538dd1c
Tysonzero <danielspencersmith@gmail.com>
2015-03-17 23:57:44 -0600

    Replace short switch case with if, else if

--- scripts/systems/render.ts
+++ scripts/systems/render.ts
@@ -7,16 +7,14 @@ module systems {
         for (i = entities.length - 1; i >= 0; i--) {
             if (entities[i] && entities[i].position && entities[i].sprite) {
                 context.fillStyle = entities[i].sprite.color;
-                switch (entities[i].sprite.shape) {
-                case 'rect':
+                if (entities[i].sprite.shape === 'rect') {
                     context.fillRect(
                         entities[i].position.x - entities[i].sprite.width / 2,
                         entities[i].position.y - entities[i].sprite.height / 2,
                         entities[i].sprite.width,
                         entities[i].sprite.height
                     );
-                    break;
-                case 'ellipse':
+                } else if (entities[i].sprite.shape === 'ellipse') {
                     context.save();
                     context.beginPath();
                     context.translate(entities[i].position.x - entities[i].sprite.width / 2,
@@ -25,7 +23,6 @@ module systems {
                     context.arc(1, 1, 1, 0, 2 * Math.PI);
                     context.fill();
                     context.restore();
-                    break;
                 }
             }
         }
