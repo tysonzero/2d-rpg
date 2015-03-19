cd7a7871b5a56ed83bdbe696779bd2dbd3794733
Tysonzero <danielspencersmith@gmail.com>
2015-03-18 23:42:43 -0600

    Add rectangular hitbox to players
    Temporary until elliptical collision is added.

--- scripts/entities.ts
+++ scripts/entities.ts
@@ -14,12 +14,14 @@ var entities: {
     {
         position: {x: 200, y: 300},
         velocity: {x: 0, y: 0},
+        hitbox: {width: 50, height: 50, shape: 'rect'},
         sprite: {width: 50, height: 50, color: '#FF0000', shape: 'ellipse'},
         walk: {acceleration: 200, speed: 200, keys: {up: 87, down: 83, left: 65, right: 68}}
     },
     {
         position: {x: 600, y: 300},
         velocity: {x: 0, y: 0},
+        hitbox: {width: 50, height: 50, shape: 'rect'},
         sprite: {width: 50, height: 50, color: '#0000FF', shape: 'ellipse'},
         walk: {acceleration: 200, speed: 200, keys: {up: 73, down: 75, left: 74, right: 76}}
     }
