a0b84fac24e1b025237cd681a72cf213de3b6c11
Tysonzero <danielspencersmith@gmail.com>
2015-03-21 23:29:05 -0600

    Get rid of bitwise operations

--- scripts/systems/collide.ts
+++ scripts/systems/collide.ts
@@ -11,6 +11,8 @@ module systems {
         var rightOverlapTime: number;
         var topOverlapTime: number;
         var bottomOverlapTime: number;
+        var velocityI: number;
+        var velocityJ: number;
         leftOverlap = entities[j].position.x - entities[i].position.x + entities[i].hitbox.width / 2 + entities[j].hitbox.width / 2;
         rightOverlap = entities[i].position.x - entities[j].position.x + entities[i].hitbox.width / 2 + entities[j].hitbox.width / 2;
         topOverlap = entities[j].position.y - entities[i].position.y + entities[i].hitbox.height / 2 + entities[j].hitbox.height / 2;
@@ -21,17 +23,19 @@ module systems {
         bottomOverlapTime = bottomOverlap / (entities[i].velocity.y - entities[j].velocity.y);
         if ((0 < leftOverlapTime && leftOverlapTime < time.delta || 0 < rightOverlapTime && rightOverlapTime < time.delta)
                 && topOverlap > 0 && bottomOverlap > 0) {
-            entities[i].velocity.x = entities[i].velocity.x ^ entities[j].velocity.x;
-            entities[j].velocity.x = entities[i].velocity.x ^ entities[j].velocity.x;
-            entities[i].velocity.x = entities[i].velocity.x ^ entities[j].velocity.x;
+            velocityI = entities[i].velocity.x;
+            velocityJ = entities[j].velocity.x;
+            entities[i].velocity.x = velocityJ;
+            entities[j].velocity.x = velocityI;
             entities[i].position.x += time.delta * entities[i].velocity.x;
             entities[j].position.x += time.delta * entities[j].velocity.x;
         }
         if ((0 < topOverlapTime && topOverlapTime < time.delta || 0 < bottomOverlapTime && bottomOverlapTime < time.delta)
                 && leftOverlap > 0 && rightOverlap > 0) {
-            entities[i].velocity.y = entities[i].velocity.y ^ entities[j].velocity.y;
-            entities[j].velocity.y = entities[i].velocity.y ^ entities[j].velocity.y;
-            entities[i].velocity.y = entities[i].velocity.y ^ entities[j].velocity.y;
+            velocityI = entities[i].velocity.y;
+            velocityJ = entities[j].velocity.y;
+            entities[i].velocity.y = velocityJ;
+            entities[j].velocity.y = velocityI;
             entities[i].position.y += time.delta * entities[i].velocity.y;
             entities[j].position.y += time.delta * entities[j].velocity.y;
         }
