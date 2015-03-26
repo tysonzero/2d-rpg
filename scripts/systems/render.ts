883a2767cab595827e53c195014bbc3fd9cd9f90
Tysonzero <danielspencersmith@gmail.com>
2015-03-25 23:34:34 -0600

    Get rid of unnecessary scale / transform calls

--- scripts/systems/render.ts
+++ scripts/systems/render.ts
@@ -8,14 +8,14 @@ module systems {
             if (entities[i] && entities[i].position && entities[i].sprite) {
                 context.fillStyle = entities[i].sprite.color;
                 if (entities[i].sprite.radius) {
-                    context.save();
                     context.beginPath();
-                    context.translate(entities[i].position.x - entities[i].sprite.radius,
-                                      entities[i].position.y - entities[i].sprite.radius);
-                    context.scale(entities[i].sprite.radius, entities[i].sprite.radius);
-                    context.arc(1, 1, 1, 0, 2 * Math.PI);
+                    context.arc(
+                        entities[i].position.x,
+                        entities[i].position.y,
+                        entities[i].sprite.radius,
+                        0, 2 * Math.PI
+                    );
                     context.fill();
-                    context.restore();
                 } else {
                     context.fillRect(
                         entities[i].position.x - entities[i].sprite.width / 2,
